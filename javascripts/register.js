958887c5603dc89365e6b343da9517903dda2862
r-konishi <r.konishi@developer.mebro.co.jp>
2016-04-23 14:25:37 +0900

    コメントの追加

--- javascripts/register.js
+++ javascripts/register.js
@@ -1,7 +1,11 @@
+// push通知が登録されているか
 var isPushEnabled = false;
 
+// window のロードイベントを追加
 window.addEventListener('load', function() {
+  // ID が push-button のエレメントを取得
   var pushButton = document.getElementById('push-button');
+  // pushButton にクリックイベントを追加
   pushButton.addEventListener('click', function() {
     if(isPushEnabled) {
       // push通知を解除する
@@ -13,4 +17,6 @@ window.addEventListener('load', function() {
       isPushEnabled = true;
     }
   });
+
+
 });
