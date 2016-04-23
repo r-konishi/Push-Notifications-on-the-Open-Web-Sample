0f48b445dfcf772c1f3ea328005a8d38574bb6dc
r-konishi <r.konishi@developer.mebro.co.jp>
2016-04-23 14:40:40 +0900

    Service Worker に対応しているか判定する処理を追加

--- javascripts/register.js
+++ javascripts/register.js
@@ -18,5 +18,23 @@ window.addEventListener('load', function() {
     }
   });
 
-
+  // Service Worker がサポートされているブラウザかどうかの判定を行う
+  if('serviceWorker' in navigator) {
+    // Service Worker に対応しているブラウザ
+    navigator.serviceWorker
+      // Service Worker の js を登録する
+      .register('./service-worker.js')
+      // Service Worker が無事登録されたら
+      .then(function(result){
+        console.log(result);
+        console.log('Service Worker の登録が完了したよ！');
+      })
+      .catch(function(error) {
+        console.log(error);
+        console.log('Service Worker の登録に失敗しちゃった！');
+      });
+  } else {
+    // Service Worker に対応していないブラウザの場合
+    console.warn('このブラウザは Service Worker に対応してないよ！');
+  }
 });
